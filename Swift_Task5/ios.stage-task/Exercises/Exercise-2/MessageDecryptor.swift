2c8ba6df92a7891637d9626add39ea9736c152b4
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-06-13 17:19:44 +0300

    init: done task

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,8 +1,41 @@
 import UIKit
 
 class MessageDecryptor: NSObject {
-    
+
+    class Item {
+        var count:Int = 0
+        var str:String = ""
+    }
+
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var stack:[Item] = []
+        let head:Item = Item()
+        var current = head
+        let arrayOfCharacters = Array(message)
+        for i in 0..<message.count{
+            let c = arrayOfCharacters[i]
+            if let digit = Int(String(c)) {
+                if i > 0 && (Int(String(arrayOfCharacters[i-1])) != nil){
+                    current.count = (current.count*10) + digit
+                }else{
+                    let nItem = Item()
+                    nItem.count = digit
+                    stack.append(current)
+                    current = nItem
+                }
+            } else if c == "[" {
+            } else if c == "]" {
+                let last = stack.popLast() ?? current
+                var str = ""
+                for _ in 0..<current.count{
+                    str += current.str
+                }
+                last.str += str
+                current = last
+            }else{
+                current.str += String(c)
+            }
+        }
+        return head.str
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -20,3 +20,4 @@ public final class Knapsack {
         return -1
     }
 }
+
